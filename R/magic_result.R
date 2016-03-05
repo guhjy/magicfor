ce6d2682d5ef1c8a132e77c58619de707e9154fe
hoxo-m <hoxo.smile@gmail.com>
2016-03-05 19:00:07 +0900

    fix

--- R/magic_result.R
+++ R/magic_result.R
@@ -9,12 +9,17 @@ magic_result <- function() {
 #'
 #' @export
 magic_result_as_vector <- function() {
-  Map(unlist, .result_env$result)
+  res <- .result_env$result
+  if(length(res) == 1) {
+    unlist(res[[1]])
+  } else {
+    Map(unlist, .result_env$result)
+  }
 }
 
 #' Get Magic Result as data.frame
 #'
 #' @export
 magic_result_as_dataframe <- function() {
-  data.frame(Map(unlist, .result_env$result))
+  data.frame(Map(I, .result_env$result))
 }
